503cb3599f7de1ba2ef8aa12820a595777db22b4
Cormac1902 <47155205+Cormac1902@users.noreply.github.com>
2019-02-05 22:08:55 +0000

    Add files via upload

--- pandas_check_data.py
+++ pandas_check_data.py
@@ -3,7 +3,7 @@ import pandas as pd
 # CSV directory and filename
 csv_dir = 'D:\\Users\\Cormac\\OneDrive\\Family\\Cormac\\College\\NUIG\\Semester 2\\Industrial Development Project\\' \
           'project_starter\\'
-file = 'test.csv'
+file = 'test2.csv'
 
 # https://towardsdatascience.com/data-cleaning-with-python-and-pandas-detecting-missing-values-3e9c6ebcf78b
 missing_values = ["N/A", "N/a", "n/A", "n/a", "Na", "nA","na", "---", "--", "-"]
@@ -15,18 +15,24 @@ def read_csv(csv_file):  # Read CSV file
 
 csv = read_csv(csv_dir + file)
 
-# Gets column labels and then stores as string
-h = csv.columns
-headers = str(h.tolist())
 
-print(h)
+def get_headers(csv_file): # Obtain headers from CSV file
+    return csv_file.columns.tolist()
+
+
+headers = get_headers(csv)
+
+# print(h)
 print(headers)
 print(csv)
 
 
 def check_upper(string):  # Check if any letter is upper case and print
-    if any(i.isupper() for i in string):
-        print('Uppercase letter found')
+    for header in string:
+        for letter in header:
+            if letter.isupper():
+                print('Uppercase letter found: ' + header)
+                break
 
 
 check_upper(headers)
